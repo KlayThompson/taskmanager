782b151ca2db6079a70d23948e9066d5cd1eee63
KlayThompson <913661647@qq.com>
2019-03-07 15:47:25 +0800

    增加header footer slider

--- taskmgr/src/app/core/core.module.ts
+++ taskmgr/src/app/core/core.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
+import {NgModule, Optional, SkipSelf} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
   imports: [
     CommonModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('模块已存在，不能再次加载！');
+    }
+  }
+}

ffa5f615e7b20c78358af631eab5561bd0843c30
KlayThompson <913661647@qq.com>
2019-04-10 15:43:17 +0800

    change form to reactiveform

--- taskmgr/src/app/login/login/login.component.ts
+++ taskmgr/src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  form = this.fb.group({
+    email: ['sunshinenate@sina.com', Validators.compose([Validators.required, Validators.email, this.emailValidate])],
+    password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+  })
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: FormGroup, ev: Event) {
+    ev.preventDefault();
+    console.log(form.valid);
+  }
+
+  emailValidate(c: FormControl): {[key: string]: any} {
+    if (!c.value) {
+      return null;
+    }
+    const pattern = /^sunshine+/;
+    if (pattern.test(c.value)) {
+      return null;
+    }
+    return {
+      emailNotValidate: 'The email must start with wang.'
+    }
+  }
 }
+
